ca6af1df97dce7375841bfed9962bd41ee47deb4
dev-ardiansyah <aa9717797@gmail.com>
2025-11-05 09:40:19 +0700

    fixing wording

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -2,17 +2,17 @@
 const { test, expect } = require('@playwright/test');
 const credentials = require('../test_data/credentials.js');
 
-test.describe('Login Page Tests', () => {
+test.describe('Login Page OpenMRS', () => {
 
-  test('should login with valid credentials', async ({ page, context, browserName }) => {
-    // Launch page
+  test('berhasil login dan masuk ke halaman utama openMRS', async ({ page, context, browserName }) => {
+   
     await page.goto('https://o2.openmrs.org/openmrs/login.htm');
 
-    // ✅ Set browser window to maximum size (for all browsers)
-    const screenSize = { width: 1920, height: 1080 }; // Full HD
+    
+    const screenSize = { width: 1920, height: 1080 }; 
     await page.setViewportSize(screenSize);
 
-    // For Chromium (Chrome, Edge), maximize window
+    
     if (browserName === 'chromium') {
       const session = await context.newCDPSession(page);
       const { windowId } = await session.send('Browser.getWindowForTarget');
@@ -22,18 +22,18 @@ test.describe('Login Page Tests', () => {
       });
     }
 
-    // Input username and password
+    
     await page.fill('#username', credentials.username);
     await page.fill('#password', credentials.password);
 
-    // Select location and click login
+    
     await page.getByText('Outpatient Clinic').click();
     await page.click('#loginButton');
 
-    // Add delay before checking redirect (for stability)
-    await page.waitForTimeout(3000); // Wait 3 seconds
+    
+    await page.waitForTimeout(3000); 
 
-    // Verify successful login
+    
     await expect(page).toHaveURL(/home\.page/);
     await expect(page.locator('h4')).toHaveText(
       'Logged in as Super User (admin) at Outpatient Clinic.',
